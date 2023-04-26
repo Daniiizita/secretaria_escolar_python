f30516ae95649d18caf88b4f882dcffc2ed5d49c
Danielle <goncalvesdani54@hotmail.com>
2023-04-26 11:01:36 -0300

    criacação main_poo.py/atualizações

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ def main():
         nome_aluno = input("Digite o nome completo do aluno: ")
         #print("O nome completo do aluno é: " + nome_aluno)
 
-        idade_aluno = input("Digite a idade do aluno: ")
+        idade_aluno = int(input("Digite a idade do aluno: "))
         #print("A idade do aluno é: " + idade_aluno)
 
         pai_aluno = input("Digite o nome do pai completo do aluno: ")
@@ -18,9 +18,27 @@ def main():
 
         mae_aluno = input("Digite o nome da mãe completo do aluno: ")
         #print("O nome completo da mãe do aluno é: " + mae_aluno)
-        
-        turma_aluno = int(input("Digite o numero da turma do aluno (obs deve ser um valor inteiro): "))
-        #print("A turma do aluno é: " + turma_aluno)
+
+        #if e else caso o aluno seja do ensino medio ou do fundamental
+
+        tipo_ensino_aluno = input("Digite o tipo de ensino do aluno (fundamental, infantil ou medio): ")
+        turma_aluno = input("Digite o numero da turma do aluno (obs deve ser um valor inteiro): ")
+
+        if tipo_ensino_aluno == "fundamental":
+            tipo_ensino_aluno = "ensino fundamental"
+            print("A turma do aluno é: " + turma_aluno + " ano do ensino fundamental")
+
+        elif tipo_ensino_aluno == "infantil":
+            tipo_ensino_aluno = "educação mnfantil"
+            print("A turma do aluno é: " + turma_aluno + " ano da educação infantil")
+
+        elif tipo_ensino_aluno == "medio":
+            tipo_ensino_aluno = "ensino médio"
+            print("A turma do aluno é: " + turma_aluno + " ano do ensino médio")
+
+        else:
+            print("Tipo de ensino inválido")
+
         
         
         #embaixo, dicionário de registro de alunos para acumulo de dados.
@@ -31,6 +49,7 @@ def main():
         aluno["pai_aluno"] = pai_aluno
         aluno["mae_aluno"] = mae_aluno
         aluno["turma_aluno"] = turma_aluno
+        aluno["tipo_ensino_aluno"] = tipo_ensino_aluno
     
     # pergunte ao usuário se deseja continuar adicionando alunos
         continuar = input("Deseja continuar adicionando alunos? (s/n) ")
---
+++ src/main_poo.py
@@ -0,0 +1,54 @@
+class Aluno:
+    def __init__(self, nome, idade, pai, mae, tipo_ensino, turma):
+        self.nome = nome
+        self.idade = idade
+        self.pai = pai
+        self.mae = mae
+        self.tipo_ensino = tipo_ensino
+        self.turma = turma
+    
+    def main():
+    print("Bem-vindo ao meu programa!")
+    
+    alunos = []  # lista vazia para armazenar as instâncias da classe Aluno
+    
+    while True:
+        nome = input("Digite o nome completo do aluno: ")
+        idade = int(input("Digite a idade do aluno: "))
+        pai = input("Digite o nome do pai completo do aluno: ")
+        mae = input("Digite o nome da mãe completo do aluno: ")
+        tipo_ensino = input("Digite o tipo de ensino do aluno (fundamental, infantil ou medio): ")
+        turma = input("Digite o numero da turma do aluno (obs deve ser um valor inteiro): ")
+        
+        aluno = Aluno(nome, idade, pai, mae, tipo_ensino, turma)  # cria uma instância da classe Aluno
+        
+        alunos.append(aluno)  # adiciona a instância do aluno à lista de alunos
+        
+        continuar = input("Deseja continuar adicionando alunos? (s/n) ")
+        if continuar.lower() == "n":
+            break
+    
+    for aluno in alunos:  # percorre a lista de alunos e exibe as informações de cada um
+        print(aluno.get_info_aluno())
+
+
+    def get_turma_ensino(self):
+        if self.tipo_ensino == "fundamental":
+            return f"{self.turma} ano do ensino fundamental"
+        elif self.tipo_ensino == "infantil":
+            return f"{self.turma} ano da educação infantil"
+        elif self.tipo_ensino == "medio":
+            return f"{self.turma} ano do ensino médio"
+        else:
+            return "Tipo de ensino inválido"
+    
+    def get_info_aluno(self):
+        return {
+            "nome": self.nome,
+            "idade": self.idade,
+            "pai": self.pai,
+            "mae": self.mae,
+            "tipo_ensino": self.tipo_ensino,
+            "turma": self.get_turma_ensino(),
+        }
+
